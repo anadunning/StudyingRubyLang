8b9c2a4a9e272d9d7b0c0726be006f63e8b67c88
Ana Dunning <anakpedrosa@hotmail.com>
2022-02-04 00:57:56 +0000

    Added new operations

--- main.rb
+++ main.rb
@@ -39,18 +39,42 @@ print "========================================================\n"
 #puts "Thank you! You said your first name is #{first_name}."
 
 # Assignment 1:
-puts "Enter your first name:"
-first_name = gets.chomp
-puts "Enter your last name:"
-last_name = gets.chomp
+#puts "Enter your first name:"
+#first_name = gets.chomp
+#puts "Enter your last name:"
+#last_name = gets.chomp
 
-full_name = "#{first_name} #{last_name}"
+#full_name = "#{first_name} #{last_name}"
 
-puts "Your full name is #{first_name} #{last_name}"
-puts "Your full name is #{full_name}."
+#puts "Your full name is #{first_name} #{last_name}"
+#puts "Your full name is #{full_name}."
 
-puts "Your full reversed name is #{full_name.reverse}."
-puts "Your name has #{full_name.length - 1} characters in it."
+#puts "Your full reversed name is #{full_name.reverse}."
+#puts "Your name has #{full_name.length - 1} characters in it."
 
+#puts 1 + 2
 
+#x = 5
+#y = 10
+#puts y / x
 
+puts "Simple calculator"
+25.times { print "-" }
+puts "Enter the first number: "
+num_1 = gets.chomp
+puts "Enter the second number: "
+num_2 = gets.chomp
+puts "The first number multiplied by the second number
+is #{num_1.to_i * num_2.to_i}. "
+
+puts "The first number added to the second number
+is #{num_1.to_i + num_2.to_i}. "
+
+puts "The first number subtracted from the second number
+is #{num_1.to_i - num_2.to_i}. "
+
+puts "The first number divided by the second number
+is #{num_1.to_i / num_2.to_i}. "
+
+puts "The first number mod the second number
+is #{num_1.to_i % num_2.to_i}. "
